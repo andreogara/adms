275a3a70c2bb35b61aa8e3aa5e06062ff16d8e8a
unknown <aogara@hawk.iit.edu>
2015-03-02 23:59:28 -0600

    fixed the problem with the list display

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,9 +1,29 @@
 var artistSpace = angular.module('artistSpace', ['ngAnimate']);
 artistSpace.controller("ListController", ['$scope', '$http', '$routeParams', function ($scope, $http, $routeParams){
+ 
+$scope.artistsOrder = "reknown";
+$scope.query = "";
+
+$scope.watchRadio = function(){
+	console.log($scope.artistsOrder);
+}
  $http.get('/data').success(function(data) {
-    $scope.artists = data;
-	$scope.artistsOrder = "name";
-  })
+ $scope.artists = data;
+ console.log(data);
+ })
+
+ $scope.dummy = [
+    { name: "Peter",   age: 20 },
+    { name: "Pablo",   age: 55 },
+    { name: "Linda",   age: 20 },
+    { name: "Marta",   age: 37 },
+    { name: "Othello", age: 20 },
+    { name: "Markus",  age: 32 }
+  ];
+$scope.filterFunction = function(element) {
+    return element.name.match(/^Ma/) ? true : false;
+  };
+
 }]);
 
 artistSpace.controller("DetailController", ['$scope', '$http', '$routeParams' , function ($scope, $http, $routeParams){
