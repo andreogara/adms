d5cecfe4e2470c5a265e632e7cf0f6380eb063a6
unknown <aogara@hawk.iit.edu>
2015-03-12 18:03:29 -0500

    Working version with Asyn calls to MongoDB and IndexedDB

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,74 +1,100 @@
 var artistSpace = angular.module('artistSpace', ['ngAnimate']);
 //Initializing the data and populating the offline database
-artistSpace.factory("dataFactory", ['$http', function($http){
-    var urlBase = "/data", //url for the api call to MongoDB instance running on AWS
-        offlineData = []; // Data from indexedDB is stored here by the cursor and should be returned to controller that calls this factory
+//Controller for the list Page
+artistSpace.controller("ListController", ['$scope', '$http', '$routeParams', 'dataFactory', function ($scope, $http, $routeParams, dataFactory){
+    $scope.artistsOrder = "reknown";
+    $scope.query = "";
+        dataFactory.getData().then(function(result){
+            $scope.artists = result.offlineData;
+        });
+
+}]);
+artistSpace.factory("dataFactory", ['$http', '$log', '$q', function($http, $log, $q) {
+    var urlBase = "/data",//url for the api call to MongoDB instance running on AWS
+        db,
+        deferred = $q.defer();
+    // Data from indexedDB is stored here by the cursor and should be returned to controller that calls this factory
     return {
-        getData : function(){
-            if(!window.indexedDB)  //if there's no indexedDB support, return a promise
-                return $http.get(urlBase); //object of the api call and let the controller handle data retrieval
+        getData: function () {
+            if (!window.indexedDB) {  //if there's no indexedDB support, return a promise
+                $http.get(urlBase) //object of the api call and let the controller handle data retrieval
+                    .success(function(data) {
+                        deferred.resolve({
+                            offlineData: data
+                        });
+                    }).error(function (msg, code) {
+                        deferred.reject(msg);
+                        $log.error(msg, code);
+                    });
+                return deferred.promise;
+            }
             else{
-                var promise = $http.get(urlBase); //if there's indexedDB support, then fetch data from api before setting up indexedDB database.
-                promise.then(function(result){ //use callback to setup indexedDB.
-                    var data = result.data;
-                    var request = indexedDB.open("daemonbits", 82);
-                    request.onerror = function(event) {
-                        console.log("Error creating IndexedDB");
-                    };
-                    request.onupgradeneeded = function(e){
-                        var db = e.target.result;
-                        if (db.objectStoreNames.contains('artists')) {
-                            db.deleteObjectStore('artists');
-                        }
-                        var storage = db.createObjectStore("artists", {autoIncrement: true});
-                        for (var i in result.data) //loop through data from api call
-                            storage.add(result.data[i]); //store data in indexedDB storage
-                        storage.openCursor().onsuccess = function(event) { //open cursor to indexedDB
+                dbSetup();
+                function dbSetup(){
+                    var version = 8,
+                        dbName = "daemonbytes",
+                        strName = "artists",
+                        request = window.indexedDB.open(dbName, version);
+                    request.onsuccess = function(e){
+                        db = e.target.result;
+                        var artistStore = db.transaction("artists", "readwrite").objectStore("artists"),
+                            offlineData = [];
+                        artistStore.openCursor().onsuccess = function(event) {
                             var cursor = event.target.result;
                             if (cursor) {
-                                offlineData.push(cursor.value);
-                                cursor.continue();
+                                 offlineData.push(cursor.value);
+                                 cursor.continue();
                             }
+                            else{
+                                deferred.resolve({
+                                    offlineData: offlineData
+                                });
+                            }
+
+                        };
+                    };
+                    request.onerror = dbError;
+                    request.onupgradeneeded = function(e){
+                        db = e.target.result;
+                        if (db.objectStoreNames.contains("artists")) {
+                            db.deleteObjectStore("artists");
                         }
-                    }
-                }, function(err){
-                    console.log(err);
-                }).then(function(){ //callback after indexedDB has been setup, populated and cursor opened
-                    return offlineData;
-                });
+                        $http.get(urlBase)
+                            .success(function(data){
+                                var artistStore = db.transaction("artists", "readwrite").objectStore("artists");
+                                for (var i in data){
+                                    artistStore.add(data[i]);
+                                }
+                            });
+                    };
+                }
+                function dbError(e){
+                    console.error("An Error has occured", e);
+                    deferred.reject(msg);
+                }
+                return deferred.promise;
             }
         }
     }
 }]);
-//Controller for the list Page
-artistSpace.controller("ListController", ['$scope', '$http', '$routeParams', '$rootScope', 'dataFactory', function ($scope, $http, $routeParams, $rootScope, dataFactory){
-    $scope.artistsOrder = "reknown";
-    $scope.query = "";
-    if (!window.indexedDB){
-        dataFactory.getData().then(function(result){
-            $scope.artists = result.data;
-        });
-    }
-    else
-        console.log(dataFactory.getData());
-}]);
-
-artistSpace.controller("DetailsController", ['$scope', '$http', '$routeParams', '$rootScope' , function ($scope, $http, $routeParams, $rootScope){
-    $scope.id = $routeParams.id;
-    $scope.artists = $rootScope.artists;
-    $scope.whichItem = $scope.id;
-    if ($scope.id > 0){
-        $scope.prevItem = Number($scope.id) - 1;
-    }
-    else {
-        $scope.prevItem = $scope.artists.length - 1;
-    }
-    if($routeParams.id < $scope.artists.length - 1){
-        $scope.nextItem = Number($scope.id) + 1;
-    }
-    else{
-        $scope.nextItem = 0;
-    }
+artistSpace.controller("DetailsController", ['$scope', '$http', '$routeParams', 'dataFactory', function ($scope, $http, $routeParams, dataFactory){
+    dataFactory.getData().then(function(result){
+        $scope.id = $routeParams.id;
+        $scope.artists = result.offlineData;
+        $scope.whichItem = $scope.id;
+        if ($scope.id > 0){
+            $scope.prevItem = Number($scope.id) - 1;
+        }
+        else {
+            $scope.prevItem = $scope.artists.length - 1;
+        }
+        if($routeParams.id < $scope.artists.length - 1){
+            $scope.nextItem = Number($scope.id) + 1;
+        }
+        else{
+            $scope.nextItem = 0;
+        }
+    });
 }]);
 //Handler for the details page
 artistSpace.controller("HomeController", ['$scope', '$http', '$routeParams' , function ($scope, $http, $routeParams){
